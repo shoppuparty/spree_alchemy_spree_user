cea74e2161cd60df75f04d85dea8caa166a44883
Thomas Mulloy <twmulloy@gmail.com>
2014-12-16 13:37:33 -0800

    user decorator

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,9 +1,9 @@
-# module Spree
-#   User.class_eval do
-#     def alchemy_roles
-#       if has_spree_role?('admin')
-#         %w(admin)
-#       end
-#     end
-#   end
-# end
+module Spree
+  User.class_eval do
+    def alchemy_roles
+      if has_spree_role?('admin')
+        %w(admin)
+      end
+    end
+  end
+end
