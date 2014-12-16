f3edc56d93d4b54b2ecfe8b359f0b7d52c96a3bf
Thomas Mulloy <twmulloy@gmail.com>
2014-12-16 12:43:29 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Spree::Core::Engine.routes.draw do
+
+  # alchemy admin routes
+  devise_scope :spree_user do
+    delete '/logout', to: 'spree/user_sessions#destroy'
+  end
+
+end
---
+++ lib/spree_alchemy_spree_user.rb
@@ -0,0 +1,2 @@
+require 'spree_core'
+require 'spree_alchemy_spree_user/engine'
