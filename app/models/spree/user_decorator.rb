2b332f5161cdd8f8ccd6f08e3d90cddd3290cb55
Thomas Mulloy <twmulloy@gmail.com>
2014-12-30 19:54:03 -0800

    admin helpers

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,9 +1,21 @@
 module Spree
   User.class_eval do
+
     def alchemy_roles
       if has_spree_role?('admin')
         %w(admin)
+      else
+        []
       end
     end
+
+    def name
+      read_attribute(:email)
+    end
+
+    def alchemy_display_name
+      "#{email}".strip
+    end
+
   end
 end
