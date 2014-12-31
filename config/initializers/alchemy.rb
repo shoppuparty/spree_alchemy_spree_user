bd7cd5b57ecf2c4823f9472ba768e9ca948afe94
Thomas Mulloy <twmulloy@gmail.com>
2014-12-31 08:31:00 -0800

    moved initializer to gem

--- config/initializers/alchemy.rb
+++ config/initializers/alchemy.rb
@@ -2,3 +2,18 @@ Alchemy.user_class_name = 'Spree::User'
 Alchemy.current_user_method = 'spree_current_user'
 Alchemy.login_path = '/login'
 Alchemy.logout_path = '/logout'
+
+
+module SpreeAlchemySpreeUser
+  class Ability
+    include CanCan::Ability
+
+    def initialize(user)
+      return if user.nil?
+      pp user
+    end
+
+  end
+end
+
+Alchemy.register_ability SpreeAlchemySpreeUser::Ability
